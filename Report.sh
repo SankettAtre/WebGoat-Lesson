76430ba78f24721741d6f5b2b40664e2deb21a1e
KritikaSri <51196851+KritikaSri@users.noreply.github.com>
2020-03-31 10:11:49 +0530

    Update Report.sh

--- Report.sh
+++ Report.sh
@@ -1,2 +1,2 @@
-cd /home/coverity/Desktop/cov-reports/bin
+cd "/home/coverity/Desktop/cov-reports/bin"
 ./cov-generate-security-report  webgoat_coverity.yaml --auth-key-file auth-key.txt --on-new-cert trust --output webgoat_coverity.pdf --project webgoat
