f2e8a2f87a96a4f49e0dc3750e75aecd4357d8c6
KritikaSri <51196851+KritikaSri@users.noreply.github.com>
2020-04-07 12:22:43 +0530

    Update sast.bat

--- sast.bat
+++ sast.bat
@@ -2,10 +2,10 @@ cd "C:\Program Files\Fortify\Fortify_SCA_and_Apps_19.1.0\bin"
 "sourceanalyzer.exe" -64 -b "webgoat" -clean
 echo 'fortify SAST clean complete'
 cd "C:\Program Files\Fortify\Fortify_SCA_and_Apps_19.1.0\bin"
-"sourceanalyzer.exe" -64 -b "calc" -source "1.8" "C:\project\WebGoat-Lessons-develop"
+"sourceanalyzer.exe" -64 -b "webgoat" -source "1.8" "C:\project\WebGoat-Lessons-develop"
 echo 'SAST translate complete'
 cd "C:\Program Files\Fortify\Fortify_SCA_and_Apps_19.1.0\bin"
-"sourceanalyzer.exe" -64 -b "calc" -format "fpr" -f "C:\project\WebGoat-Lessons-develop\webgoat.fpr" -scan
+"sourceanalyzer.exe" -64 -b "webgoat" -format "fpr" -f "C:\project\WebGoat-Lessons-develop\webgoat.fpr" -scan
 echo 'SAST scan complete'
 cd "C:\Program Files\Fortify\Fortify_SCA_and_Apps_19.1.0\bin"
 ReportGenerator.bat -template "DeveloperWorkbook.xml"  -format "pdf" -f "C:\project\WebGoat-Lessons-develop\webgoat.pdf" -source "C:\project\WebGoat-Lessons-develop\webgoat.pdf"
