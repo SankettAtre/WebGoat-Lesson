5bffcaee089205563515051d3ccee61016b129e1
KritikaSri <51196851+KritikaSri@users.noreply.github.com>
2020-04-10 17:56:42 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 cd "/opt"
-"java -jar synopsys-detect-6.1.0.jar --blackduck.url=https://infosys.app.blackduck.com/ --blackduck.username=sysadmin --blackduck.password=DevSecOps12 --detect.test.connection=true --logging.level.com.synopsys.integration=TRACE --blackduck.proxy.host=10.74.91.103 --blackduck.proxy.port=80 --blackduck.proxy.username=Kritika_S --blackduck.proxy.password=Indu%40123"
+java -jar synopsys-detect-6.1.0.jar --blackduck.url=https://infosys.app.blackduck.com/ --blackduck.username=sysadmin --blackduck.password=DevSecOps12 --detect.test.connection=true --logging.level.com.synopsys.integration=TRACE --blackduck.proxy.host=10.74.91.103 --blackduck.proxy.port=80 --blackduck.proxy.username=Kritika_S --blackduck.proxy.password=Indu%40123
